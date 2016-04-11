61d308b71345f28f1f2300002fedd71ccf504069
Michael Heumann <michael@mce-business.de>
2016-04-11 16:36:09 +0200

    adds Inlines to Admin

--- docmgr/admin.py
+++ docmgr/admin.py
@@ -1,30 +1,38 @@
 # coding: utf-8
 
-"""
-    docmgr.admin
-    Copyright (C) 2015-2016  Michael Heumann
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
 from django.contrib import admin
+from django.contrib.contenttypes.admin import (
+    GenericStackedInline,
+    GenericTabularInline
+)
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Document
 from .forms import DocumentAdminForm
 
 
+class DocumentStackedInline(GenericStackedInline):
+    form = DocumentAdminForm
+    model = Document
+    extra = 1
+
+
+class DocumentTabularInline(GenericTabularInline):
+    form = DocumentAdminForm
+    model = Document
+    extra = 1
+
+
 class DocumentAdmin(admin.ModelAdmin):
     form = DocumentAdminForm
+    list_display = ['pk', 'docfile', 'description', 'content_type', 'object_id', 'featured_image']
+
+    def featured_image(self, obj):
+        return format_html(
+            '<img src="%s" style="max-width: 180px; max-height: 150px;" />' % (obj.docfile.url)
+        )
+    featured_image.short_description = _(u'Featured Image')
 
 
 admin.site.register(Document, DocumentAdmin)
