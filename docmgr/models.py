2aca09919ea83a51334cb3a6b74f1d8665b62fc1
Michael Heumann <michael@mce-business.de>
2016-04-20 02:59:59 +0200

    updates model to ensure a proper filename adds properties for filename and path

--- docmgr/models.py
+++ docmgr/models.py
@@ -1,3 +1,4 @@
+import os
 import time
 import uuid
 
@@ -5,6 +6,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.files.storage import FileSystemStorage
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
@@ -14,16 +16,23 @@ docstorage = FileSystemStorage(location=UPLOAD_PATH)
 
 
 def get_upload_path(instance, filename):
-    return "{structure}/{file}".format(structure=time.strftime('%Y'), file=filename)
+    """
+    cleans filename and returns the current year and
+    the cleaned filename as upload location
+    """
+    fname, dot, extension = filename.rpartition('.')
+    slugged_filename = slugify(fname)
+    slugged = '%s.%s' % (slugged_filename, extension)
+    return "{structure}/{file}".format(structure=time.strftime('%Y'), file=slugged)
 
 
 class Document(models.Model):
     uuid = models.UUIDField(primary_key=True,
                             default=uuid.uuid4, editable=False)
     docfile = models.FileField(
+        _('Document File'),
         upload_to=get_upload_path,
         storage=docstorage,
-        help_text=_('Click on fieldname or image to open the upload dialog.')
     )
     description = models.TextField(
         _('Description'),
@@ -36,5 +45,15 @@ class Document(models.Model):
 
     uploaded_at = models.DateTimeField(default=timezone.now, editable=False)
 
-    def filename(self):
+    @property
+    def filepath(self):
+        """
+        returns the structured path with the filename
+        defaults to <currentyear>/<slugifiedfilename>
+        """
         return self.docfile.name
+
+    @property
+    def filename(self):
+        """ returns the (slugified) filename only """
+        return os.path.basename(self.docfile.name)
