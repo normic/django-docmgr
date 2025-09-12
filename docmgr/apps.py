c5a7b93fb4fff3e2d82fefe33e1afbd01110b3c9
Michael Heumann <michael@vilanet.de>
2025-09-12 12:08:50 +0200

    set default_auto_field, update .gitignore, and simplify MANIFEST.in

--- docmgr/apps.py
+++ docmgr/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class DocMgrConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'docmgr'
     verbose_name = 'Document Manager'
 
